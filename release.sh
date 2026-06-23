4a95a6b13ee33420371c0ff266ec2b911cbb33b0
Rayhan Alcena <llcoolray24@gmail.com>
2026-06-22 22:46:10 -0400

    updated the release to make sure the verion number is correct upon release to itch.io

--- release.sh
+++ release.sh
@@ -2,14 +2,14 @@
 # release.sh
 VERSION=$1  # optional: pass version tag
 
-dotnet publish -c Release -r linux-x64   --self-contained -o ./builds/linux
-dotnet publish -c Release -r win-x64     --self-contained -o ./builds/windows
-dotnet publish -c Release -r osx-x64     --self-contained -o ./builds/mac-intel
-dotnet publish -c Release -r osx-arm64   --self-contained -o ./builds/mac-arm
+dotnet publish -c Release -r linux-x64   --self-contained -p:PublishSingleFile=true -o ./builds/linux
+dotnet publish -c Release -r win-x64     --self-contained -p:PublishSingleFile=true -o ./builds/windows
+dotnet publish -c Release -r osx-x64     --self-contained -p:PublishSingleFile=true -o ./builds/mac-intel
+dotnet publish -c Release -r osx-arm64   --self-contained -p:PublishSingleFile=true -o ./builds/mac-arm
 
-butler push ./builds/linux      iamllcoolray/pong:linux
-butler push ./builds/windows    iamllcoolray/pong:windows
-butler push ./builds/mac-intel  iamllcoolray/pong:mac-intel
-butler push ./builds/mac-arm    iamllcoolray/pong:mac-arm
+butler push ./builds/linux      iamllcoolray/pong:linux --userversion "${VERSION:-untagged}"
+butler push ./builds/windows    iamllcoolray/pong:windows --userversion "${VERSION:-untagged}"
+butler push ./builds/mac-intel  iamllcoolray/pong:mac-intel --userversion "${VERSION:-untagged}"
+butler push ./builds/mac-arm    iamllcoolray/pong:mac-arm --userversion "${VERSION:-untagged}"
 
 echo "Done! Version: ${VERSION:-untagged}"
